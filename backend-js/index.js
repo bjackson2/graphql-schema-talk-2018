94c8fb39f81f54817b3ce03ab67e8ae2e7878c3b
Ben Jackson <bjackson2@gmail.com>
2018-11-18 21:02:49 -0500

    Adds express, schema dump endpoint to backend-js

--- backend-js/index.js
+++ backend-js/index.js
@@ -1,9 +1,18 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./src/typeDefs");
+const express = require("express");
+const { buildSchema, printSchema } = require("graphql");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { schema } = require("./src/schema");
 const { resolvers } = require("./src/resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs: gql(schema), resolvers });
+const app = express();
 
-server.listen(3002).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+app.get("/graphql/schema", (req, res) => {
+  return res.send(printSchema(buildSchema(schema)));
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 3002 }, () => {
+  console.log(`🚀  Server ready at ${server.graphqlPath}`);
 });
--- backend-js/src/schema.js
+++ backend-js/src/schema.js
@@ -1,6 +1,4 @@
-const { gql } = require("apollo-server");
-
-module.exports.typeDefs = gql`
+module.exports.schema = `
   type Artist {
     bio: String
     id: ID!
